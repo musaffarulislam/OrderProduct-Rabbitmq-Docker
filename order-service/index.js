4ccc56f87405463cf54f1e33970a1d908d7c1a61
musaffarulislam <musaffarulislam@gmail.com>
2023-05-15 01:12:36 +0530

    order service to add try catch

--- order-service/index.js
+++ order-service/index.js
@@ -3,34 +3,28 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 const amqp = require("amqplib");
 const Order = require("./models/Order");
-const connectDB = require("./config/database")
+const connectDB = require("./config/database");
 
-connectDB()
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-var channel, connection;
-
-// mongoose
-//   .connect("mongodb://0.0.0.0:27017/scan-order-service", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("Order-Service Connected to MongoDB"))
-//   .catch((e) => console.log(e));
-
-// RabbitMQ connection
-
+let channel, connection;
 
 async function connectToRabbitMQ() {
-  const amqpServer = "amqp://guest:guest@rabbitmq:5672";
-  connection = await amqp.connect(amqpServer);
-  channel = await connection.createChannel();
-  await channel.assertQueue("order-service-queue");
-  await channel.assertQueue("order-product-queue");
+  try {
+    const amqpServer = "amqp://guest:guest@rabbitmq:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("order-service-queue");
+    await channel.assertQueue("order-product-queue");
+    console.log("Order Service RabbitMQ connected");
+  } catch (err) {
+    console.error("Error:", err.message);
+    process.exit(1); // Terminate the application if the RabbitMQ connection fails
+  }
 }
 
-// Create an order
-createOrder = async (products) => {
+async function createOrder(products) {
   let total = 0;
   products.forEach((product) => {
     total += product.price;
@@ -42,39 +36,50 @@ createOrder = async (products) => {
   });
   await order.save();
   return order;
-};
-
-productOrders = async (productId) =>{
-  const orders = await Order.find({})
-  const ordersWithProduct = orders.filter(order => order.productIds.includes(productId));
-  console.log("Order Service: ", ordersWithProduct);
-  return ordersWithProduct;
 }
 
-connectToRabbitMQ().then(() => {
-  channel.consume("order-service-queue", (data) => {
-    // order service queue listens to this queue
-    const { products } = JSON.parse(data.content);
-    console.log(products);
-    const newOrder = createOrder(products);
-    channel.ack(data);
-    channel.sendToQueue(
-      "product-service-queue",
-      Buffer.from(JSON.stringify(newOrder))
-    );
-  });
+async function productOrders(productId) {
+  const orders = await Order.find();
+  const ordersWithProduct = orders.filter((order) =>
+    order.productIds.includes(productId)
+  );
+  console.log("Order Service:", orders);
+  return orders;
+}
 
-  channel.consume('order-product-queue', (data)=>{
-    const {product } = JSON.parse(data.content);
-    const orders = productOrders(product);
-    channel.ack(data);
-    channel.sendToQueue(
-      'product-order-queue',
-      Buffer.from(JSON.stringify(orders))
-    )
-  })
-});
+// Move the connection logic inside an async IIFE
+(async () => {
+  try {
+    await connectToRabbitMQ();
 
-app.listen(PORT, () => {
-  console.log(`Order-Service listening on port ${PORT}`);
-});
+    channel.consume("order-service-queue", async (data) => {
+      const { products } = JSON.parse(data.content);
+      console.log("Products:", products);
+      const newOrder = await createOrder(products);
+      channel.ack(data);
+      channel.sendToQueue(
+        "product-service-queue",
+        Buffer.from(JSON.stringify(newOrder))
+      );
+    });
+
+    channel.consume("order-product-queue", async (data) => {
+      const { product } = JSON.parse(data.content);
+      console.log("Product ID:", product);
+      const orders = await productOrders(product);
+      console.log("Orders:", orders);
+      channel.ack(data);
+      channel.sendToQueue(
+        "product-order-queue",
+        Buffer.from(JSON.stringify(orders))
+      );
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Order-Service listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    process.exit(1); // Terminate the application if an error occurs
+  }
+})();
--- product-service/routes/routes.js
+++ product-service/routes/routes.js
@@ -6,28 +6,17 @@ const amqp = require("amqplib");
 let order, channel, connection;
 
 // Connect to RabbitMQ
+
 async function connectToRabbitMQ() {
-  const amqpServer = "amqp://guest:guest@rabbitmq:5672";
-  const maxRetries = 5;
-  let retryCount = 0;
-  
-  while (retryCount < maxRetries) {
-    try {
-      connection = await amqp.connect(amqpServer);
-      channel = await connection.createChannel();
-      await channel.assertQueue("product-service-queue");
-      await channel.assertQueue("product-order-queue");
-      break; // Connection successful, exit the loop
-    } catch (error) {
-      console.error("Failed to connect to RabbitMQ. Retrying...");
-      retryCount++;
-      await new Promise((resolve) => setTimeout(resolve, 2000)); // Wait for 2 seconds before retrying
-    }
-  }
-  
-  if (retryCount === maxRetries) {
-    console.error("Failed to connect to RabbitMQ after maximum retries.");
-    // Handle the failure scenario accordingly
+  try{
+    const amqpServer = "amqp://guest:guest@rabbitmq:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("product-service-queue");
+    await channel.assertQueue("product-order-queue");
+    console.log("Product Service Rabbitmq connect");
+  }catch(err){
+    console.error("Error : ", err.message);
   }
 }
 
@@ -35,76 +24,102 @@ connectToRabbitMQ();
 
 // Create a new product
 router.post("/", async (req, res) => {
-  const { name, price, description } = req.body;
-  if (!name || !price || !description) {
-    return res.status(400).json({
-      message: "Please provide name, price and description",
+  try{
+    const { name, price, description } = req.body;
+    if (!name || !price || !description) {
+      return res.status(400).json({
+        message: "Please provide name, price and description",
+      });
+    }
+    const product = await new Product({ ...req.body });
+    await product.save();
+    return res.status(201).json({
+      message: "Product created successfully",
+      product,
+    });
+  }catch(err){
+    console.error("Error : ", err.message);
+    res.status(400).json({
+      message: "Product created Error",
     });
   }
-  const product = await new Product({ ...req.body });
-  await product.save();
-  return res.status(201).json({
-    message: "Product created successfully",
-    product,
-  });
 });
 
 // Buy a product
 router.post("/buy", async (req, res) => {
-  const { productIds } = req.body;
-  const products = await Product.find({ _id: { $in: productIds } });
-  if (!connection || !channel) {
-    await connectToRabbitMQ();
+  try{
+    const { productIds } = req.body;
+    const products = await Product.find({ _id: { $in: productIds } });
+    if (!connection || !channel) {
+      await connectToRabbitMQ();
+    }
+  
+    // Send order to RabbitMQ order queue
+    channel.sendToQueue(
+      "order-service-queue",
+      Buffer.from(
+        JSON.stringify({
+          products
+        })
+      )
+    );
+  
+    // Consume previously placed order from RabbitMQ & acknowledge the transaction
+    channel.consume("product-service-queue", (data) => {
+      console.log("Consumed from product-service-queue");
+      order = JSON.parse(data.content);
+      console.log("Order: ",order);
+      channel.ack(data);
+    });
+  
+    // Return a success message
+    return res.status(201).json({
+      message: "Order placed successfully",
+      order: order,
+    });
+  }catch(err){
+    console.error("Error : ", err.message);
+    res.status(400).json({
+      message: "Product buy Error",
+    });
   }
-  // Send order to RabbitMQ order queue
-  channel.sendToQueue(
-    "order-service-queue",
-    Buffer.from(
-      JSON.stringify({
-        products
-      })
-    )
-  );
-
-  // Consume previously placed order from RabbitMQ & acknowledge the transaction
-  channel.consume("product-service-queue", (data) => {
-    console.log("Consumed from product-service-queue");
-    order = JSON.parse(data.content);
-    console.log("Order: ",order);
-    channel.ack(data);
-  });
-
-  // Return a success message
-  return res.status(201).json({
-    message: "Order placed successfully",
-    order: order,
-  });
 });
 
 
 router.get("/productOrders", async (req, res)=>{
-  const {productId } = req.body;
-  if (!connection || !channel) {
-    await connectToRabbitMQ();
+  try{
+    const {productId } = req.body;
+    if (!connection || !channel) {
+      await connectToRabbitMQ();
+      console.log("connect Rabbitmq");
+    }
+    
+    channel.sendToQueue(
+      'order-product-queue',
+      Buffer.from(
+        JSON.stringify({
+          productId
+        })
+      )
+    );
+    
+    channel.consume("product-order-queue", (data) =>{
+      console.log("Consumed from product-order-queue");
+      order = JSON.parse(data.content);
+      console.log("Order: ",order);
+      channel.ack(data);
+    })
+  
+    return res.status(201).json({
+      message: "Order return successfully",
+      order: order
+    })
+  }catch(err){
+    console.error("Error : ", err.message);
+    res.status(400).json({
+      message: "Product Orders Error",
+    });
   }
-  channel.sendToQueue(
-    'order-product-queue',
-    Buffer.from(
-      JSON.stringify({
-        productId
-      })
-    )
-  );
-  channel.consume("product-order-queue", (data) =>{
-    console.log("Consumed from product-order-queue");
-    order = JSON.parse(data.content);
-    channel.ack(data);
-  })
-
-  return res.status(201).json({
-    message: "Order return successfully",
-    order
-  })
 })
 
 module.exports = router;
